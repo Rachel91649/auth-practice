37115a0578205ef24b7b090571fbe740f79862f9
Rachel91649 <rachelnjackson@yahoo.com>
2018-03-01 17:30:11 -0500

    set up app.js and got db connected

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//DEPENDENCIES
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -5,6 +6,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//AUTH DEPENDENCIES
+const session = require('express-session');//creates session cookies.
+const passport = require('passport');//authenticates.
+const LocalStrategy = require('passport-local').Strategy;//works with passport to authenticate and log users in.
+
+//REQUIRED ROUTES
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -20,11 +27,36 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+//your auth functions need go here under cookieParser --- because order matters.
+app.use(session({
+  secret: "\x02\xf3\xf7r\t\x9f\xee\xbbu\xb1\xe1\x90\xfe'\xab\xa6L6\xdd\x8d[\xccO\xfe",//In procudtion make sure to save the session token as a variable
+  resave: false, 
+  saveUnitialized: true
+}));
+//^^^this needs to be used before app.use(passport.session()) to ensure that the login session is restored in the correct order. 
+app.use(passport.initialize());//this initializes passport
+app.use(passport.session());//this is used for persisten login sessions
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+//ROUTES
+app.use('/', index);//This is the root route.
 app.use('/users', users);
 
+
+//PASSPORT CONFIGURATION
+//I am not sure of how to implement what Michael Herman has in his tutorial here.  Below is what he has
+/*
+let Account = require('./models/account');//this is requiring the mongo database modelSchema for an account
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.desrializeUser(Account.deserializeUser());
+*/
+
+//ERROR HANDLERS
+
+// This is Dev error handler
+// Will print stacktrace
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -32,6 +64,8 @@ app.use(function(req, res, next) {
   next(err);
 });
 
+// This is Production error handler
+// No stacktraces leaked to user
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
